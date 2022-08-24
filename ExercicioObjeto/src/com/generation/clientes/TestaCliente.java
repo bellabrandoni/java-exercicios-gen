45483e8e6dcfb48d3a7f89d2c38f5ed9f6056289
unknown <isabella14brandao@gmail.com>
2022-08-24 17:19:03 -0300

    Finalizando exercicio objetos

---
+++ ExercicioObjeto/src/com/generation/clientes/TestaCliente.java
@@ -0,0 +1,15 @@
+package com.generation.clientes;
+
+import com.generation.cliente.model.Clientes;
+
+public class TestaCliente {
+
+	public static void main(String[] args) {
+		
+		/*Instanciando Objeto*/
+		Clientes c1 = new Clientes (1,"233445aa","Isabella","Rua Carlos Marques","isabella@gmail",11982493);
+		c1.vizualizar();
+	}
+	
+	
+}
